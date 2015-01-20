1a8ddfad73995eca1cbb2164120465ae403e0e06
Andy Meerwaldt <andy@yoast.com>
2015-01-20 14:33:35 +0100

    Extend the correct class

--- tests/test-class-admin-page.php
+++ tests/test-class-admin-page.php
@@ -1,6 +1,6 @@
 <?php
 
-class WPSEO_News_Admin_Page_Test extends WPSEO_UnitTestCase {
+class WPSEO_News_Admin_Page_Test extends WPSEO_News_UnitTestCase {
 
 	public function test_for_test() {
 		$this->assertTrue( true );
