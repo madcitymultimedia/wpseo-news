17a5ce717e09708e534ed386225a04eb1e920ae2
Andy Meerwaldt <andy@yoast.com>
2015-02-04 08:53:46 +0100

    Separate the HTML into class method to keep it clear. Probably a view would be better.

--- classes/class-admin-page.php
+++ classes/class-admin-page.php
@@ -1,10 +1,12 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-} // Exit if accessed directly
-
 class WPSEO_News_Admin_Page {
+
+	/**
+	 * Holder for object for admin_pages
+	 * @var
+	 */
+	private $wpseo_admin_pages;
 
 	/**
 	 * Display admin page
@@ -12,8 +14,7 @@ class WPSEO_News_Admin_Page {
 	public function display() {
 		global $wpseo_admin_pages;
 
-		// Load options
-		$options = WPSEO_News::get_options();
+		$this->wpseo_admin_pages = $wpseo_admin_pages;
 
 		// Admin header
 		$wpseo_admin_pages->admin_header( true, 'yoast_wpseo_news_options', 'wpseo_news' );
@@ -29,33 +30,63 @@ class WPSEO_News_Admin_Page {
 		echo $wpseo_admin_pages->textinput( 'name', __( 'Google News Publication Name', 'wordpress-seo-news' ) );
 
 		// Default Genre
-		echo $wpseo_admin_pages->select( 'default_genre', __( 'Default Genre', 'wordpress-seo-news' ),
-			WPSEO_News::list_genres()
-		);
+		echo $wpseo_admin_pages->select( 'default_genre', __( 'Default Genre', 'wordpress-seo-news' ), WPSEO_News::list_genres() );
 
 		// Default keywords
-		echo $wpseo_admin_pages->textinput( 'default_keywords', __( 'Default Keywords', 'wordpress-seo-news' ) );
-		echo '<p>' . __( 'It might be wise to add some of Google\'s suggested keywords to all of your posts, add them as a comma separated list. Find the list here:', 'wordpress-seo-news' ) . ' ' . make_clickable( 'http://www.google.com/support/news_pub/bin/answer.py?answer=116037' ) . '</p>';
-
-		echo $wpseo_admin_pages->checkbox( 'restrict_sitemap_featured_img', __( 'Only use featured image for XML News sitemap, ignore images in post.', 'wordpress-seo-news' ), false );
-
-		echo '<br><br>';
+		$this->default_keywords();
 
 		// Post Types to include in News Sitemap
-		echo '<h2>' . __( 'Post Types to include in News Sitemap', 'wordpress-seo-news' ) . '</h2>';
-		foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $posttype ) {
-			echo $wpseo_admin_pages->checkbox( 'newssitemap_include_' . $posttype->name, $posttype->labels->name, false );
-		}
+		$this->include_post_types();
 
 		// Post categories to exclude
+		$this->excluded_post_categories( );
+
+		// Editors' Pick
+		$this->editors_pick();
+
+		// Admin footer
+		$wpseo_admin_pages->admin_footer( true, false );
+	}
+
+	/**
+	 * Generate HTML for the keywords which will be defaulted
+	 */
+	private function default_keywords() {
+		echo $this->wpseo_admin_pages->textinput( 'default_keywords', __( 'Default Keywords', 'wordpress-seo-news' ) );
+		echo '<p>' . __( 'It might be wise to add some of Google\'s suggested keywords to all of your posts, add them as a comma separated list. Find the list here:', 'wordpress-seo-news' ) . ' ' . make_clickable( 'http://www.google.com/support/news_pub/bin/answer.py?answer=116037' ) . '</p>';
+
+		echo $this->wpseo_admin_pages->checkbox( 'restrict_sitemap_featured_img', __( 'Only use featured image for XML News sitemap, ignore images in post.', 'wordpress-seo-news' ), false );
+
+		echo '<br><br>';
+	}
+
+	/**
+	 * Generate HTML for the post types which should be included in the sitemap
+	 */
+	private function include_post_types() {
+		echo '<h2>' . __( 'Post Types to include in News Sitemap', 'wordpress-seo-news' ) . '</h2>';
+		foreach ( get_post_types( array( 'public' => true ), 'objects' ) as $post_type ) {
+			echo $this->wpseo_admin_pages->checkbox( 'newssitemap_include_' . $post_type->name, $post_type->labels->name, false );
+		}
+	}
+
+	/**
+	 * Generate HTML for excluding post categories
+	 */
+	private function excluded_post_categories( ) {
+		$options = WPSEO_News::get_options();
 		if ( isset( $options['newssitemap_include_post'] ) ) {
 			echo '<h2>' . __( 'Post categories to exclude', 'wordpress-seo-news' ) . '</h2>';
 			foreach ( get_categories() as $cat ) {
-				echo $wpseo_admin_pages->checkbox( 'catexclude_' . $cat->slug, $cat->name . ' (' . $cat->count . ' posts)', false );
+				echo $this->wpseo_admin_pages->checkbox( 'catexclude_' . $cat->slug, $cat->name . ' (' . $cat->count . ' posts)', false );
 			}
 		}
+	}
 
-		// Post Types to include in News Sitemap
+	/**
+	 * Part with HTML for editors pick
+	 */
+	private function editors_pick() {
 		echo '<h2>' . __( "Editors' Pick", 'wordpress-seo-news' ) . '</h2>';
 
 		$esc_form_key = 'ep_image_src';
@@ -69,10 +100,6 @@ class WPSEO_News_Admin_Page {
 
 		echo '<p>' . sprintf( __( 'You can find your Editors\' Pick RSS feed here: %1$sEditors\' Pick RSS Feed%2$s', 'wordpress-seo-news' ), "<a target='_blank' class='button-secondary' href='" . home_url( 'editors-pick.rss' ) . "'>", '</a>' ) . '</p>';
 		echo '<p>' . sprintf( __( 'You can submit your Editors\' Pick RSS feed here: %1$sSubmit Editors\' Pick RSS Feed%2$s', 'wordpress-seo-news' ), "<a class='button-secondary' href='https://support.google.com/news/publisher/contact/editors_picks' target='_blank'>", '</a>' ) . '</p>';
-
-		// Admin footer
-		$wpseo_admin_pages->admin_footer( true, false );
-
 	}
 
 }
