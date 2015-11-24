71fc4785782ceea110be154b9b25b0271dc80b8d
Andy Meerwaldt <andy@yoast.com>
2015-11-24 14:18:07 +0100

    Update bootstrap for the tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,13 +8,18 @@ if ( function_exists( 'xdebug_disable' ) ) {
 echo 'Welcome to the Wordpress SEO News test suite' . PHP_EOL;
 echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
 
+if( false !== getenv( 'WP_PLUGIN_DIR' ) ) {
+	define( 'WP_PLUGIN_DIR', getenv( 'WP_PLUGIN_DIR' ) );
+}
+
 $GLOBALS['wp_tests_options'] = array(
 	'active_plugins' => array( 'wordpress-seo/wp-seo.php', 'wpseo-news/wpseo-news.php' ),
 );
 
 if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	require getenv( 'WP_DEVELOP_DIR' ) . 'tests/phpunit/includes/bootstrap.php';
-} else {
+}
+else {
 	require '../../../../tests/phpunit/includes/bootstrap.php';
 }
 
