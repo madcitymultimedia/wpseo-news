58bedf4bec1f02b217b7f280484efa07a82c9167
Anton Timmermans <anton@yoast.com>
2016-02-26 11:21:59 +0100

    Add helpscout suggestion articles

--- classes/class-beacon-setting.php
+++ classes/class-beacon-setting.php
@@ -13,7 +13,14 @@ class WPSEO_News_Beacon_Setting implements Yoast_HelpScout_Beacon_Setting {
 	public function get_suggestions( $page ) {
 		switch ( $page ) {
 			case 'wpseo_news':
-				return array();
+				return array(
+					'538308c0e4b087d9517672fe',
+					// See: http://kb.yoast.com/article/110-configuration-guide-for-news-seo.
+					'53901dc1e4b044eb1018e206',
+					// See: http://kb.yoast.com/article/133-publication-date-time-in-the-news-xml-sitemap.
+					'5375e852e4b03c6512282d5a',
+					// See: http://kb.yoast.com/article/36-my-sitemap-is-blank-what-s-wrong.
+				);
 				break;
 		}
 
