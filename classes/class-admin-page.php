6740391d4d4c760682977305a06697f7d6988774
Caroline Geven <caroline@yoast.com>
2016-03-07 13:44:15 +0100

    Written tests

--- classes/class-admin-page.php
+++ classes/class-admin-page.php
@@ -2,15 +2,21 @@
 
 class WPSEO_News_Admin_Page {
 
+	private $options;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		$this->options = WPSEO_News::get_options();
+
+		$this->register_i18n_promo_class();
+	}
+
 	/**
 	 * Display admin page
 	 */
 	public function display() {
-		// Load options
-		$options = WPSEO_News::get_options();
-
-		$this->register_i18n_promo_class();
-
 		// Admin header
 		WPSEO_News_Wrappers::admin_header( true, 'yoast_wpseo_news_options', 'wpseo_news' );
 
@@ -49,6 +55,26 @@ class WPSEO_News_Admin_Page {
 	}
 
 	/**
+	 * Register the promotion class for our GlotPress instance.
+	 *
+	 * @link https://github.com/Yoast/i18n-module
+	 */
+	protected function register_i18n_promo_class() {
+		new yoast_i18n(
+			array(
+				'textdomain'     => 'yoast-video-seo',
+				'project_slug'   => 'yoast-video-seo',
+				'plugin_name'    => 'WordPress SEO News',
+				'hook'           => 'yoast_news_seo_admin_footer',
+				'glotpress_url'  => 'http://translate.yoast.com/gp/',
+				'glotpress_name' => 'Yoast Translate',
+				'glotpress_logo' => 'http://translate.yoast.com/gp-templates/images/Yoast_Translate.svg',
+				'register_url'   => 'http://translate.yoast.com/gp/projects#utm_source=plugin&utm_medium=promo-box&utm_campaign=wpseo-news-i18n-promo',
+			)
+		);
+	}
+
+	/**
 	 * Generate HTML for the keywords which will be defaulted
 	 */
 	private function default_keywords() {
@@ -75,8 +101,7 @@ class WPSEO_News_Admin_Page {
 	 * Generate HTML for excluding post categories
 	 */
 	private function excluded_post_categories() {
-		$options = WPSEO_News::get_options();
-		if ( isset( $options['newssitemap_include_post'] ) ) {
+		if ( isset( $this->options['newssitemap_include_post'] ) ) {
 			echo '<h2>' . __( 'Post categories to exclude', 'wordpress-seo-news' ) . '</h2>';
 			foreach ( get_categories() as $cat ) {
 				echo WPSEO_News_Wrappers::checkbox( 'catexclude_' . $cat->slug, $cat->name . ' (' . $cat->count . ' posts)', false );
@@ -91,35 +116,14 @@ class WPSEO_News_Admin_Page {
 		echo '<h2>' . __( "Editors' Pick", 'wordpress-seo-news' ) . '</h2>';
 
 		$esc_form_key = 'ep_image_src';
-		$options      = WPSEO_News::get_options();
 
 		echo '<label class="select" for="' . $esc_form_key . '">' . __( "Editors' Pick Image", 'wordpress-seo-news' ) . ':</label>';
-		echo '<input id="' . $esc_form_key . '" type="text" size="36" name="wpseo_news[' . $esc_form_key . ']" value="' . esc_attr( $options[ $esc_form_key ] ) . '" />';
+		echo '<input id="' . $esc_form_key . '" type="text" size="36" name="wpseo_news[' . $esc_form_key . ']" value="' . esc_attr( $this->options[ $esc_form_key ] ) . '" />';
 		echo '<input id="' . $esc_form_key . '_button" class="wpseo_image_upload_button button" type="button" value="' . __( 'Upload Image', 'wordpress-seo-news' ) . '" />';
 		echo '<br class="clear"/>';
 
 		echo '<p>' . sprintf( __( 'You can find your Editors\' Pick RSS feed here: %1$sEditors\' Pick RSS Feed%2$s', 'wordpress-seo-news' ), "<a target='_blank' class='button-secondary' href='" . home_url( 'editors-pick.rss' ) . "'>", '</a>' ) . '</p>';
 		echo '<p>' . sprintf( __( 'You can submit your Editors\' Pick RSS feed here: %1$sSubmit Editors\' Pick RSS Feed%2$s', 'wordpress-seo-news' ), "<a class='button-secondary' href='https://support.google.com/news/publisher/contact/editors_picks' target='_blank'>", '</a>' ) . '</p>';
-	}
-
-	/**
-	 * Register the promotion class for our GlotPress instance.
-	 *
-	 * @link https://github.com/Yoast/i18n-module
-	 */
-	private function register_i18n_promo_class() {
-		new yoast_i18n(
-			array(
-				'textdomain'     => 'yoast-video-seo',
-				'project_slug'   => 'yoast-video-seo',
-				'plugin_name'    => 'WordPress SEO News',
-				'hook'           => 'yoast_news_seo_admin_footer',
-				'glotpress_url'  => 'http://translate.yoast.com/gp/',
-				'glotpress_name' => 'Yoast Translate',
-				'glotpress_logo' => 'http://translate.yoast.com/gp-templates/images/Yoast_Translate.svg',
-				'register_url'   => 'http://translate.yoast.com/gp/projects#utm_source=plugin&utm_medium=promo-box&utm_campaign=wpseo-news-i18n-promo',
-			)
-		);
 	}
 }
 
