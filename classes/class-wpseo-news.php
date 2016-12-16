3f92160cca3232703fec130784ec67cc3992b7e5
Caroline Geven <caroline@yoast.com>
2016-12-16 10:35:27 +0100

    Code style fix - hopefully

--- classes/class-wpseo-news.php
+++ classes/class-wpseo-news.php
@@ -261,10 +261,10 @@ class WPSEO_News {
 	 */
 	public function error_upgrade_wp() {
 		echo '<div class="error"><p>',
-		     sprintf(
-		     	/* translators: %1$s resolves to News SEO */
-		     	__( 'Please upgrade WordPress to the latest version to allow WordPress and the %1$s module to work properly.', 'wordpress-seo-news' ),
-		        'News SEO'
+			sprintf(
+		        /* translators: %1$s resolves to News SEO */
+		        __( 'Please upgrade WordPress to the latest version to allow WordPress and the %1$s module to work properly.', 'wordpress-seo-news' ),
+				'News SEO'
 		     ), '</p></div>';
 	}
 
