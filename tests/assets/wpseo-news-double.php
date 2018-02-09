e4e39d12f436f9d0aa1119d113a89ef450bcafcb
Jip <jhp.moors@gmail.com>
2018-02-09 16:03:11 +0100

    Add missing new line

--- tests/assets/wpseo-news-double.php
+++ tests/assets/wpseo-news-double.php
@@ -7,12 +7,10 @@
  * Class WPSEO_News_Test
  */
 class WPSEO_News_Double extends WPSEO_News {
-
 	/**
 	 *v @inheritdoc
 	 */
 	public function check_dependencies( $wp_version ) {
 		return parent::check_dependencies( $wp_version );
 	}
-
 }
