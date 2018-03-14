cafd55f487d0686be6ffb2b21ce0c68a26fb6198
jrfnl <jrfnl@users.noreply.github.com>
2018-03-14 02:27:18 +0100

    CS/QA: make sure $_SERVER array key is set before using it

--- classes/class-meta-box.php
+++ classes/class-meta-box.php
@@ -28,7 +28,9 @@ class WPSEO_News_Meta_Box extends WPSEO_Metabox {
 		add_filter( 'wpseo_save_metaboxes', array( $this, 'save' ), 10, 1 );
 		add_action( 'add_meta_boxes', array( $this, 'add_tab_hooks' ) );
 
-		if ( $pagenow === 'post.php' || $pagenow === 'post-new.php' || stristr( $_SERVER['REQUEST_URI'], '/news-sitemap.xml' ) ) {
+		if ( $pagenow === 'post.php' || $pagenow === 'post-new.php'
+			|| ( isset( $_SERVER['REQUEST_URI'] ) && stristr( $_SERVER['REQUEST_URI'], '/news-sitemap.xml' ) )
+		) {
 			add_filter( 'add_extra_wpseo_meta_fields', array( $this, 'add_meta_fields_to_wpseo_meta' ) );
 		}
 	}
