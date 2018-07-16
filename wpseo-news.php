6917db35b8ed56115c4fceee27f4ef434a2d2d43
Jip <jhp.moors@gmail.com>
2018-07-16 14:14:37 +0200

    Update copyright end-year to current year

--- wpseo-news.php
+++ wpseo-news.php
@@ -17,7 +17,7 @@
  * License:     GPL v3
  *
  * Yoast SEO Plugin
- * Copyright (C) 2008-2016, Team Yoast
+ * Copyright (C) 2008-2018, Team Yoast
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
