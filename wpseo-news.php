97314335944dd725dfae2da383f8b60428b930e0
Jimmy Comack <jimmy@yoast.com>
2018-07-24 09:57:30 +0200

    Bumped versions for release

--- wpseo-news.php
+++ wpseo-news.php
@@ -6,7 +6,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO: News
- * Version:     7.8
+ * Version:     7.9
  * Plugin URI:  https://yoast.com/wordpress/plugins/news-seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseonewsplugin
  * Description: Google News plugin for the Yoast SEO plugin
  * Author:      Team Yoast
