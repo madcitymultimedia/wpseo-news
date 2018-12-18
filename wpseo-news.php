3ec6edd2e34b8f6b239d98b0892a711210bfd415
IreneStr <irene@yoast.com>
2018-12-18 14:07:19 +0100

    Preparations for 9.4-RC1

--- wpseo-news.php
+++ wpseo-news.php
@@ -6,7 +6,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO: News
- * Version:     9.4-beta1
+ * Version:     9.4-RC1
  * Plugin URI:  https://yoast.com/wordpress/plugins/news-seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseonewsplugin
  * Description: Google News plugin for the Yoast SEO plugin
  * Author:      Team Yoast
