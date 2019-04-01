206d78d7432d2470a2cc0561a539af4fa7e8de22
Joost de Valk <joost@yoast.com>
2019-04-01 08:44:54 +0200

    call get_included_post_types later

--- classes/class-schema.php
+++ classes/class-schema.php
@@ -13,7 +13,6 @@ class WPSEO_News_Schema {
 	 * WPSEO_News_Head Constructor.
 	 */
 	public function __construct() {
-		$this->post_types = WPSEO_News::get_included_post_types();
 		add_filter( 'wpseo_schema_article_post_types', array( $this, 'article_post_types' ) );
 		add_filter( 'wpseo_schema_article', array( $this, 'change_article' ) );
 	}
@@ -26,7 +25,7 @@ class WPSEO_News_Schema {
 	 * @return array $post_types Supported post types.
 	 */
 	public function article_post_types( $post_types ) {
-		$post_types = array_merge( $this->post_types, $post_types );
+		$post_types = array_merge( WPSEO_News::get_included_post_types(), $post_types );
 
 		return $post_types;
 	}
@@ -40,7 +39,7 @@ class WPSEO_News_Schema {
 	 */
 	public function change_article( $data ) {
 		$post = get_post();
-		if ( in_array( $post->post_type, $this->post_types ) ) {
+		if ( in_array( $post->post_type, WPSEO_News::get_included_post_types() ) ) {
 			$data['@type']           = 'NewsArticle';
 			$data['copyrightYear']   = mysql2date( 'Y', $post->post_date_gmt, false );
 			$data['copyrightHolder'] = array( '@id' => WPSEO_Utils::get_home_url() . WPSEO_Schema_IDs::ORGANIZATION_HASH );
