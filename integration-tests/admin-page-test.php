f924968a6a60a3a50df00b8ec4309d9657091853
jrfnl <jrfnl@users.noreply.github.com>
2020-12-07 01:12:16 +0100

    Tests: use `expectOutputContains()`
    WP Test Utils introduces a new `expectOutputContains()` method to complement the PHPUnit native `expectOutput*()` methods as it appears testing whether output contains a certain text string is a common pattern in the Yoast tests.
    
    By default this method will ignore differences in line endings to improve the cross-OS compatibility of the tests.
    
    This switches out the custom code to do the same in this test suite in favour of using the `expectOutputContains()` method instead.

--- integration-tests/admin-page-test.php
+++ integration-tests/admin-page-test.php
@@ -40,20 +40,13 @@ class WPSEO_News_Admin_Page_Test extends WPSEO_News_UnitTestCase {
 	 */
 	public function test_display() {
 
-		// Start buffering to get the output of display method.
-		ob_start();
-
-		$this->instance->display();
-
-		$output = ob_get_contents();
-		ob_end_clean();
-
 		// We expect this part in the generated HTML.
 		$expected_output = <<<'EOT'
 <p>You will generally only need a News Sitemap when your website is included in Google News.</p><p><a target="_blank" href="http://example.org/news-sitemap.xml">View your News Sitemap</a>.</p>
 EOT;
 
-		// Check if the $output contains the $expected_output.
-		$this->assertStringContainsString( $expected_output, $output );
+		$this->expectOutputContains( $expected_output );
+
+		$this->instance->display();
 	}
 }
