d4b0d434a269f828b82b84f41564b1e7eadf183d
marijnyoast <marijn@yoast.com>
2021-02-18 11:42:41 +0100

    register alert identifier

--- classes/wpseo-news.php
+++ classes/wpseo-news.php
@@ -58,6 +58,9 @@ class WPSEO_News {
 		add_filter( 'wpseo_helpscout_beacon_settings', [ $this, 'filter_helpscout_beacon' ] );
 
 		add_filter( 'wpseo_frontend_presenters', [ $this, 'add_frontend_presenter' ] );
+
+		$editor_reactification_alert = new WPSEO_Video_Editor_Reactification_Alert();
+		$editor_reactification_alert->register_hooks();
 	}
 
 	/**
