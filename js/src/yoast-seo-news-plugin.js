873c2deac9f064f6a6c9c0cfc369ca502c9df587
Igor <35524806+igorschoester@users.noreply.github.com>
2021-02-18 11:42:12 +0100

    Load JS translations
    Deprecate old translations

--- js/src/yoast-seo-news-plugin.js
+++ js/src/yoast-seo-news-plugin.js
@@ -1,6 +1,8 @@
 import domReady from "@wordpress/dom-ready";
 import initializeEditorStore from "./initializers/initializeEditorStore";
+import initializeTranslations from "./initializers/initializeTranslations";
 
 domReady( () => {
+	initializeTranslations();
 	initializeEditorStore();
 } );
