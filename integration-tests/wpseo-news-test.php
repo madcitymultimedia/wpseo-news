1b6a1474e060f8723b3b78a32c7ad7e469ecb33b
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-10-14 09:59:40 +0200

    Update integrations tests

--- integration-tests/wpseo-news-test.php
+++ integration-tests/wpseo-news-test.php
@@ -54,12 +54,12 @@ class WPSEO_News_Test extends WPSEO_News_UnitTestCase {
 			[ false, '12.7', '5.5', 'WordPress is below the minimal required version.' ],
 			[ false, false, '5.3', 'WordPress SEO is not installed.' ],
 			[ false, '8.1', '5.6', 'WordPress SEO is below the minimal required version.' ],
-			[ false, '16.6', '5.7', 'WordPress SEO is below the minimal required version.' ],
-			[ false, '16.9-RC5', '5.7', 'WordPress SEO is below the minimal required version.' ],
-			[ true, '16.9-RC6', '5.8', 'WordPress and WordPress SEO have the minimal required versions.' ],
-			[ true, '16.9-RC6', '5.7', 'WordPress and WordPress SEO have the minimal required versions.' ],
-			[ true, '16.9-RC6', '5.6', 'WordPress and WordPress SEO have the minimal required versions.' ],
-			[ true, '16.9', '5.8', 'WordPress and WordPress SEO have the minimal required versions.' ],
+			[ false, '16.9', '5.7', 'WordPress SEO is below the minimal required version.' ],
+			[ false, '17.4-RC2', '5.7', 'WordPress SEO is below the minimal required version.' ],
+			[ true, '17.4-RC3', '5.8', 'WordPress and WordPress SEO have the minimal required versions.' ],
+			[ true, '17.4-RC3', '5.7', 'WordPress and WordPress SEO have the minimal required versions.' ],
+			[ true, '17.4-RC3', '5.6', 'WordPress and WordPress SEO have the minimal required versions.' ],
+			[ true, '17.4', '5.8', 'WordPress and WordPress SEO have the minimal required versions.' ],
 		];
 	}
 }
