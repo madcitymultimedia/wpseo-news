a76dd638c8a9aeb31a39d4825a2ca3bc68f302a1
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-06-16 11:29:18 +0200

    Add unit test for the constructor + some bootstrap definitions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,6 +7,16 @@
 
 define( 'WPSEO_INDEXABLES', true );
 define( 'WPSEO_NEWS_VERSION', '12.6' );
+define( 'WPSEO_VERSION', '17.6' );
+define( 'YOAST_SEO_NEWS_WP_REQUIRED', '6.1' );
+
+if ( ! defined( 'WPSEO_PATH' ) ) {
+	define( 'WPSEO_PATH', dirname( dirname( __DIR__ ) ) . '/' );
+}
+
+if ( ! defined( 'WPSEO_FILE' ) ) {
+	define( 'WPSEO_FILE', WPSEO_PATH . 'wp-seo.php' );
+}
 
 if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) === false ) {
 	echo PHP_EOL, 'ERROR: Run `composer install` to generate the autoload files before running the unit tests.', PHP_EOL;
